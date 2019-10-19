43453a7b9a904a54bb19007605500ad851c1d836
simmontali <sim.montali@gmail.com>
2019-10-19 11:26:22 +0200

    [🚀]We're back to public & open source!

--- README.md
+++ README.md
@@ -1,2 +1,109 @@
-# visuaspy
-Instagram spies catcher.
+<!-- PROJECT LOGO -->
+<br />
+<p align="center">
+  <a href="https://github.com/simmontali/visuaspy">
+    <img src="res/sunglasses.png" alt="Logo" width="130" height="130">
+  </a>
+  <h1 align="center">VisuaSpy</h1>
+
+  <p align="center">
+    Instagram spies catcher.  
+  </p>
+</p>
+
+
+
+<!-- TABLE OF CONTENTS -->
+## Table of Contents
+
+- [Table of Contents](#table-of-contents)
+- [About The Project](#about-the-project)
+  - [How it works](#how-it-works)
+- [Getting Started](#getting-started)
+  - [Updates](#updates)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+- [Contact](#contact)
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+![Product Name Screen Shot][screenshot]
+
+**VisuaSpy** was born to find your Instagram *stalkers*, a.k.a. users that view your stories but don't follow you. It is meant to be used as a cronjob. In the past, experiments using the algorithm in a public bot were done. People didn't use it so here we are back on the public side of GitHub!
+### How it works
+**VisuaSpy** uses the [Instagram Private API project](https://github.com/ping/instagram_private_api) to fetch your story viewers. It then compares it to your followers/following, and only presents it if you're not following each other, but you have mutual followers *(Hating those fashion bloggers with story viewing bots)*.
+
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+You can just clone this repository, install the requirements by running
+```
+$ pip3 install -r requirements.txt
+```
+and then start VisuaSpy.
+
+### Updates
+Pull this repository for updates.
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+To run VisuaSpy with a Telegram bot, run
+```
+ $ python3 stalkers.py -u "[YOUR_USER_HERE]" -p "[YOUR_PASSWORD_HERE]" -settings "[COOKIE_FILENAME_HERE]" -t "[TELEGRAM_BOT_API_KEY]" -c [YOUR_TELEGRAM_CHAT_ID]
+```
+These parameters are:
+* `-u` your Instagram user
+* `-p` your Instagram password
+* `-settings` your cookie file name. For the first run, you can insert a random `name.json`.
+* `-t` is your Telegram Bot API key, get it from the BotFather.
+* `-c` is your Telegram chat ID. It will be printed on the console when you `\start` the bot.
+
+To run VisuaSpy without Telegram, just run
+```
+ $ python3 stalkers.py -u "[YOUR_USER_HERE]" -p "[YOUR_PASSWORD_HERE]" -settings "[COOKIE_FILENAME_HERE]"
+```
+with these parameters:
+* `-u` your Instagram user
+* `-p` your Instagram password
+* `-settings` your cookie file name. For the first run, you can insert a random `name.json`.
+
+
+
+<!-- CONTRIBUTING -->
+## Contributing
+
+Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+
+## Stalkers cleaning
+```
+"stalkers": \[[a-zA-Z.\-_\d", ]*\]
+```
+<!-- LICENSE -->
+## License
+
+Distributed under the GPL License. See `LICENSE` for more information.
+<div>Icon made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a></div>
+
+
+<!-- CONTACT -->
+## Contact
+
+[Simone Montali](https://monta.li)
+
+Project Link: [https://github.com/simmontali/VisuaSpy](https://github.com/simmontali/visuaspy)
+
+[screenshot]: res/screenshot.gif "Screenshot"
+[logo]: res/sunglasses.png
---
+++ stalkers.py
@@ -0,0 +1,184 @@
+import json
+import codecs
+import datetime
+import os.path
+import logging
+import argparse
+import telegram.ext
+from telegram.ext import Updater
+from telegram.ext import MessageHandler, Filters, CommandHandler
+from telegram import (User, Message, Update, Chat, ChatMember, UserProfilePhotos, File,
+                      ReplyMarkup, TelegramObject, WebhookInfo, GameHighScore, StickerSet,
+                      PhotoSize, Audio, Document, Sticker, Video, Animation, Voice, VideoNote,
+                      Location, Venue, Contact, InputFile, ParseMode, KeyboardButton, ReplyKeyboardMarkup,
+                      InlineKeyboardButton, ChatAction, Bot, CallbackQuery, InlineKeyboardMarkup, ForceReply)
+try:
+    from instagram_private_api import (
+        Client, ClientError, ClientLoginError,
+        ClientCookieExpiredError, ClientLoginRequiredError,
+        __version__ as client_version)
+except ImportError:
+    import sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+    from instagram_private_api import (
+        Client, ClientError, ClientLoginError,
+        ClientCookieExpiredError, ClientLoginRequiredError,
+        __version__ as client_version)
+
+
+def to_json(python_object):
+    if isinstance(python_object, bytes):
+        return {'__class__': 'bytes',
+                '__value__': codecs.encode(python_object, 'base64').decode()}
+    raise TypeError(repr(python_object) + ' is not JSON serializable')
+
+
+def from_json(json_object):
+    if '__class__' in json_object and json_object['__class__'] == 'bytes':
+        return codecs.decode(json_object['__value__'].encode(), 'base64')
+    return json_object
+
+
+def onlogin_callback(api, new_settings_file):
+    cache_settings = api.settings
+    with open(new_settings_file, 'w') as outfile:
+        json.dump(cache_settings, outfile, default=to_json)
+        print('SAVED: {0!s}'.format(new_settings_file))
+
+
+def start (update, context):
+    print(update.message.chat_id)
+    context.bot.send_message(chat_id=update.message.chat_id, text="Correctly printed your chat ID\n")
+    
+
+
+#updater.idle()
+
+if __name__ == '__main__':
+
+    logging.basicConfig()
+    logger = logging.getLogger('instagram_private_api')
+    logger.setLevel(logging.WARNING)
+
+    # Example command:
+    # python examples/savesettings_logincallback.py -u "yyy" -p "zzz" -settings "test_credentials.json"
+    parser = argparse.ArgumentParser(description='login callback and save settings demo')
+    parser.add_argument('-settings', '--settings', dest='settings_file_path', type=str, required=True)
+    parser.add_argument('-u', '--username', dest='username', type=str, required=True)
+    parser.add_argument('-p', '--password', dest='password', type=str, required=True)
+    parser.add_argument('-t', '--token', dest='token', type=str)
+    parser.add_argument('-c', '--chat', dest='chat', type=str)
+
+
+    parser.add_argument('-debug', '--debug', action='store_true')
+    args = parser.parse_args()
+    if(args.token != None):
+        updater = Updater(token=args.token, use_context=True)
+        dispatcher = updater.dispatcher
+        dispatcher.add_handler(CommandHandler("start", start))
+        updater.start_polling()
+
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+
+    print('Client version: {0!s}'.format(client_version))
+
+    device_id = None
+    try:
+
+        settings_file = args.settings_file_path
+        if not os.path.isfile(settings_file):
+            # settings file does not exist
+            print('Unable to find file: {0!s}'.format(settings_file))
+
+            # login new
+            api = Client(
+                args.username, args.password,
+                on_login=lambda x: onlogin_callback(x, args.settings_file_path))
+        else:
+            with open(settings_file) as file_data:
+                cached_settings = json.load(file_data, object_hook=from_json)
+            print('Reusing settings: {0!s}'.format(settings_file))
+
+            device_id = cached_settings.get('device_id')
+            # reuse auth settings
+            api = Client(
+                args.username, args.password,
+                settings=cached_settings)
+
+    except (ClientCookieExpiredError, ClientLoginRequiredError) as e:
+        print('ClientCookieExpiredError/ClientLoginRequiredError: {0!s}'.format(e))
+
+        # Login expired
+        # Do relogin but use default ua, keys and such
+        api = Client(
+            args.username, args.password,
+            device_id=device_id,
+            on_login=lambda x: onlogin_callback(x, args.settings_file_path))
+
+    except ClientLoginError as e:
+        print('ClientLoginError {0!s}'.format(e))
+        exit(9)
+    except ClientError as e:
+        print('ClientError {0!s} (Code: {1:d}, Response: {2!s})'.format(e.msg, e.code, e.error_response))
+        exit(9)
+    except Exception as e:
+        print('Unexpected Exception: {0!s}'.format(e))
+        exit(99)
+
+    # Show when login expires
+    cookie_expiry = api.cookie_jar.auth_expires
+    print('Cookie Expiry: {0!s}'.format(datetime.datetime.fromtimestamp(cookie_expiry).strftime('%Y-%m-%dT%H:%M:%SZ')))
+    saved_stalkers = []
+    if not os.path.isfile('stalkers.txt'):
+        open ('stalkers.txt', 'w+')
+        print("stalkers.txt not existing. Creating it.")
+    saved_stalkers = [line.rstrip('\n') for line in open('stalkers.txt')]
+    print(saved_stalkers)
+    story_feed = api.user_story_feed(api.authenticated_user_id)
+    my_feed = story_feed.get('reel', [])
+    uuid = api.generate_uuid()
+    my_followers = []
+    followers_result = api.user_followers(api.authenticated_user_id, uuid)
+    my_followers.extend(followers_result['users'])
+    last_id = followers_result['next_max_id']
+    while (last_id != None):
+        followers_result = api.user_followers(api.authenticated_user_id, uuid, max_id = last_id)
+        my_followers.extend(followers_result['users'])
+        last_id = followers_result['next_max_id']
+    my_followers_ids = []
+    for follower in my_followers:
+        my_followers_ids.append(follower['pk'])
+    for stor in my_feed.get('items', []):
+        viewers_result = api.story_viewers(stor.get('id', []))
+        story_viewers = viewers_result['users']
+        last_id = viewers_result['next_max_id']
+        while (last_id != None):
+            viewers_result = api.story_viewers(stor.get('id', []), max_id = last_id)
+            story_viewers.extend(viewers_result['users'])
+            last_id = viewers_result['next_max_id']
+        for stalker in story_viewers:
+            if (not stalker['friendship_status']['following']):
+                if not stalker['pk'] in my_followers_ids:
+                    user_info = api.user_info(stalker['pk'])
+                    if (user_info['user']['mutual_followers_count']>1):
+                        if(args.token != None):
+                            if not user_info['user']['username'] in saved_stalkers:
+                                updater.bot.send_photo(chat_id=args.chat, photo = user_info['user']['hd_profile_pic_versions'][1]['url'], caption="Found a stalker!\n" + "Username: "+user_info['user']['username']+"\nFull name: "+user_info['user']['full_name']+"\nFollowers: "+str(user_info['user']['follower_count'])+"\nFollowing: "+str(user_info['user']['following_count']))
+                                saved_stalkers.append(user_info['user']['username'])
+                        else:
+                            if not user_info['user']['username'] in saved_stalkers:
+                                print("Found a stalker!\n" + "Username: "+user_info['user']['username']+"\nFull name: "+user_info['user']['full_name']+"\nFollowers: "+str(user_info['user']['follower_count'])+"\nFollowing: "+str(user_info['user']['following_count']))
+                                saved_stalkers.append(user_info['user']['username'])
+    print(saved_stalkers)
+    # Saving and closing
+    with open('stalkers.txt', 'w') as stalkers_file:
+        for saving_stalker in saved_stalkers:
+            stalkers_file.write(saving_stalker+"\n")
+
+    if (args.token!=None):
+        updater.stop()
+        updater.is_idle = False
+    print('All ok')
+
+
